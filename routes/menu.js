ed928a233cb80fac74778874809ec2b946d16238
tufailzaman <tufailzaman789@gmail.com>
2023-10-11 15:58:03 +0500

    exam added

--- routes/menu.js
+++ routes/menu.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const router = express.Router();
 
+router.get('/exam', (req, res, next )=>{
+ 
+    res.sendFile(path.join(__dirname, '../', 'views','exam.html'));
+
+});
+
 router.get('/', (req, res, next) => {
 
     res.sendFile(path.join(__dirname,'../', 'views','index.html'))
